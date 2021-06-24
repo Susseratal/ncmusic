c27b91a25ee51541044654c0ea29f2f4e18eaf82
Iain <jtfjyrx@gmail.com>
2021-06-24 19:57:05 +0100

    added default conf file

---
+++ src/conf.py
@@ -0,0 +1,16 @@
+#########################################################################
+#                                 _                         __ _        #
+#  _ __   ___ _ __ ___  _   _ ___(_) ___    ___ ___  _ __  / _(_) __ _  #
+# | '_ \ / __| '_ ` _ \| | | / __| |/ __|  / __/ _ \| '_ \| |_| |/ _` | #
+# | | | | (__| | | | | | |_| \__ \ | (__  | (_| (_) | | | |  _| | (_| | #
+# |_| |_|\___|_| |_| |_|\__,_|___/_|\___|  \___\___/|_| |_|_| |_|\__, | #
+#                                                                |___/  #
+#########################################################################
+
+class config():
+    MPV_Path = "/usr/bin/mpv" # Linux path
+    # MPV_Path = "/usr/local/bin/mpv" # MacOS path
+    # Uncomment as necessary for your computer or correct based on where you've installed it
+    Music_Path = "/path/to/your/music"
+    # Use an absolute path to your music directory. It should then be formatted:
+    # music > artists > albums > songs
